274d5b1242bb9a49d541a1bdc89a013b299461af
bhavesh <bhavesh.khubnani@gmail.com>
2023-10-21 00:39:31 -0700

    third commit

--- handler.py
+++ handler.py
@@ -12,8 +12,8 @@ logger.setLevel(logging.INFO)
 input_bucket = "cc-ss-input-2"
 output_bucket = "cc-ss-output-2"
 s3 = boto3.client('s3')
+# file_path = '/home/app/encoding'
 file_path = 'encoding'
-
 
 # Function to read the 'encoding' file
 def open_encoding(filename):
@@ -25,19 +25,25 @@ def open_encoding(filename):
 
 def face_recognition_handler(event, context):
     try:
+        logger.info(event)
+        logger.info(context)
         logger.info("Lambda function executed.")
-        all_face_encodings = open_encoding(file_path).values()
-        #print(all_face_encodings)
-        logger.info(all_face_encodings)
+        encoding_dict=open_encoding(file_path)
+        encoding_names=encoding_dict['name']
+        encoding_values = encoding_dict['encoding']
+
         for record in event['Records']:
 
             bucket = record['s3']['bucket']['name']
             key = record['s3']['object']['key']
+            logger.info(bucket)
+            logger.info(key)
 
             frame_dir = "/tmp/frames/"
             os.makedirs(frame_dir, exist_ok=True)
-            video_path = f"/tmp/{key}"
-            frame_paths = []
+            video_path = "/tmp/"+key
+            video_dir = '/'.join(video_path.split('/')[:-1])
+            os.makedirs(video_dir, exist_ok=True)
 
             # Download the video from S3
             s3.download_file(bucket, key, video_path)
@@ -55,14 +61,13 @@ def face_recognition_handler(event, context):
 
             for frame_file in frame_files:
                 frame_image = face_recognition.load_image_file(os.path.join(frame_dir, frame_file))
-                unknown_face = face_recognition.face_encodings(frame_image)
-                result = face_recognition.compare_faces(all_face_encodings, unknown_face)
-
-                if result:
-                    break
+                unknown_face_encoding = face_recognition.face_encodings(frame_image)
+                for index, value in enumerate(encoding_values):
+                    if value == unknown_face_encoding:
+                        result= index
 
             if result:
-                print(f"First face detected for {key}! Result : {result}")
+                print(f"First face detected for {key}! Name of the person identified is  : {encoding_names[result]}")
             else:
                 print(f"No faces detected for : {key}")
 
@@ -77,17 +82,43 @@ def face_recognition_handler(event, context):
         }
     except Exception as e:
         raise e
-# if __name__ == '__main__':
-#     event={
-#         "Records" :{
-#             "s3" : {
-#                 "bucket" : {
-#                     "name" : "s3://cc-ss-input-2"
-#                 },
-#                 "object" : {
-#                     "key" : "raw/test_0.mp4"
-#                 }
-#             }
-#         }
-#     }
-#     print(face_recognition_handler(event,[]))
+if __name__ == '__main__':
+    event={
+  "Records": [
+    {
+      "eventVersion": "2.1",
+      "eventSource": "aws:s3",
+      "awsRegion": "us-east-1",
+      "eventTime": "2023-10-21T06:32:01.378Z",
+      "eventName": "ObjectCreated:Put",
+      "userIdentity": {
+        "principalId": "A2UJIRMU9PGJGP"
+      },
+      "requestParameters": {
+        "sourceIPAddress": "72.201.57.106"
+      },
+      "responseElements": {
+        "x-amz-request-id": "RQPX9N2KAS9XEDSM",
+        "x-amz-id-2": "SfMX+LhadYEOTOU5BnmDyTxe/w/ZgOH0RnvL6ccnz0AngKNgVzgf7/JGVL9kCuPv7QGfSgLxuQm1M0CQ3f0GSjtkEJY1ORkZ2fBUyoGzZJc="
+      },
+      "s3": {
+        "s3SchemaVersion": "1.0",
+        "configurationId": "c9361b17-de7b-4df9-b9fa-2193e28a4f41",
+        "bucket": {
+          "name": "cc-ss-input-2",
+          "ownerIdentity": {
+            "principalId": "A2UJIRMU9PGJGP"
+          },
+          "arn": "arn:aws:s3:::cc-ss-input-2"
+        },
+        "object": {
+          "key": "raw/test_8.mp4",
+          "size": 624083,
+          "eTag": "1c6a7147ac6d63f70b0fb8fd4d691e72",
+          "sequencer": "0065337061452A496C"
+        }
+      }
+    }
+  ]
+}
+    print(face_recognition_handler(event,[]))
